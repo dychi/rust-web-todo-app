045aba9d21c0016d4b1535b23d1172f7f2b4e29e
daichi_yamaoka <tnychor06@gmail.com>
2022-12-23 01:26:51 +0900

    axum=0.6だとcreate_todo::<T>で発生するためバージョン変更

---
+++ src/handlers.rs
@@ -0,0 +1,46 @@
+use std::sync::Arc;
+use axum::{
+	Json,
+	extract::{Extension, Path},
+	http::StatusCode,
+	response::IntoResponse,
+};
+
+use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
+
+pub async fn create_todo<T: TodoRepository>(
+    Json(payload): Json<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    let todo = repository.create(payload);
+
+    (StatusCode::CREATED, Json(todo))
+}
+
+pub async fn find_todo<T: TodoRepository> (
+	Path(_id): Path<i32>,
+	Extension(_repository): Extension<Arc<T>>
+) -> Result<impl IntoResponse, StatusCode> {
+	Ok(StatusCode::OK)
+}
+
+pub async fn all_todo<T: TodoRepository> (
+	Extension(_repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+	todo!()
+}
+
+pub async fn update_todo<T: TodoRepository> (
+	Path(_id): Path<i32>,
+	Json(_payload): Json<UpdateTodo>,
+	Extension(_repository): Extension<Arc<T>>,
+) -> Result< impl IntoResponse, StatusCode> {
+	Ok(StatusCode::OK)
+}
+
+pub async fn delete_todo<T: TodoRepository> (
+	Path(_id): Path<i32>,
+	Extension(_repository): Extension<Arc<T>>
+) -> StatusCode {
+	todo!()
+}
