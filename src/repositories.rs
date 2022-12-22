5e02cd89052b05b3ffaea4c4b4f3d36d7528610c
daichi_yamaoka <tnychor06@gmail.com>
2022-12-23 02:41:14 +0900

    chapter3の実装

--- src/repositories.rs
+++ src/repositories.rs
@@ -5,6 +5,7 @@ use std::{
 use anyhow::{Context, Ok};
 use thiserror::Error;
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
 #[derive(Debug, Error)]
 enum RepositoryError {
@@ -29,13 +30,17 @@ pub struct Todo {
     completed: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct CreateTodo {
+	#[validate(length(min=1, message="Can not be empty"))]
+	#[validate(length(max=100, message="Over text length"))]
     text: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Validate)]
 pub struct UpdateTodo {
+	#[validate(length(min=1, message="Can not be empty"))]
+	#[validate(length(max=100, message="Over text length"))]
     text: Option<String>,
     completed: Option<bool>,
 }
